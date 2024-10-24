6197c3b501f452250a447e1c68472e4881344347
Youssef Hachicha <yousseftakihachicha@gmail.com>
2024-10-24 09:19:40 +0100

    added joyFill Manger, and fixed issues

--- composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/events/joyfill/ChangeFactory.kt
+++ composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/events/joyfill/ChangeFactory.kt
@@ -1,5 +1,10 @@
 package com.youssef.kotlinflowts.events.joyfill
 
+import com.youssef.kotlinflowts.events.joyfill.internal.ChangeEventImpl
+import com.youssef.kotlinflowts.events.joyfill.internal.ChangeLogImpl
+import com.youssef.kotlinflowts.events.joyfill.internal.FieldUpdateChangeImpl
+import com.youssef.kotlinflowts.events.joyfill.internal.UnknownChangeImpl
+
 fun MutableMap<String, Any?>.toChangeLog(): ChangeLog = ChangeLogImpl(this)
 
 fun MutableMap<String, Any?>.toChangeEvent(): ChangeEvent = ChangeEventImpl(this)
--- composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/manager/joyfill/FieldEvent.kt
+++ composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/manager/joyfill/FieldEvent.kt
@@ -1,5 +1,8 @@
 package com.youssef.kotlinflowts.manager.joyfill
 
+import com.youssef.kotlinflowts.models.joyfill.Page
+import com.youssef.kotlinflowts.models.joyfill.fields.Field
+
 class FieldEvent(
     val field: Field,
     val page: Page
