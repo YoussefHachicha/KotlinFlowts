81e5ac14c51030a61a1211748b0668159b69a349
Youssef Hachicha <yousseftakihachicha@gmail.com>
2024-11-17 11:46:39 +0100

    save

--- composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/app/gallery/GallerySelector.kt
+++ composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/app/gallery/GallerySelector.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -24,6 +28,9 @@ fun GallerySelector(
     onExpandChange: (Boolean) -> Unit = {},
     add: (LayoutBuilder?) -> Unit
 ) {
+    val columns by remember(layoutComponents) { mutableStateOf(layoutComponents.filter {it.type == Component.Type.column }) }
+    val rows by remember(layoutComponents) { mutableStateOf(layoutComponents.filter {it.type == Component.Type.row }) }
+
     if (expanded) Dialog(
         onDismissRequest = { onExpandChange(false) },
         properties = DialogProperties(usePlatformDefaultWidth = false),
@@ -33,8 +40,14 @@ fun GallerySelector(
                 modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp).fillMaxSize(0.9f)
             ) {
                 LazyColumn {
-                    items(layoutComponents) { layout ->
-                        GalleryItem(layout.title) {
+                    itemsIndexed(columns) { index, layout ->
+                        GalleryItem("${layout.title} #${index + 1}") {
+                            onExpandChange(false)
+                            add(builders[layout.id])
+                        }
+                    }
+                    itemsIndexed(rows) { index, layout ->
+                        GalleryItem("${layout.title} #${index + 1}") {
                             onExpandChange(false)
                             add(builders[layout.id])
                         }
