f5891270f3def1e99bdf6ea2e550c5365e287d96
Youssef Hachicha <yousseftakihachicha@gmail.com>
2024-11-26 08:07:06 +0100

    save

--- composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/youssef/kotlinflowts/app/App.kt
@@ -4,21 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.youssef.kotlinflowts.app.editor.EditorSample
 import com.youssef.kotlinflowts.app.gallery.ComponentsGallerySample
@@ -104,4 +96,3 @@ fun App() {
         }
     }
 }
-
